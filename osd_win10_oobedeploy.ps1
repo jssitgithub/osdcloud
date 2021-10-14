fd89edb01ec29713703bf110daf0d539b51865bf
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2021-10-14 10:18:51 +0200

    Update osd_win10_oobedeploy.ps1

--- osd_win10_oobedeploy.ps1
+++ osd_win10_oobedeploy.ps1
@@ -1,1 +1,7 @@
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+
+Import-Module OSD -Force
+
 Start-OOBEDeploy -UpdateWindows -RemoveAppx "CommunicationsApps,OfficeHub,People,Skype,Solitaire,Xbox,ZuneMusic,ZuneVideo"
