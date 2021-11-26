10b98a689c1aebaf247dde438bd3c69c2a4d5cb5
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2021-11-26 08:06:51 +0100

    Create osd_win11_setup.ps1

---
+++ osd_win11_setup.ps1
@@ -0,0 +1,7 @@
+## change the default "Start-OSDCloud" behaviour from Windows 10 to Windows 11
+$osdlatest = Get-InstalledModule -Name OSD
+$fullpath = Join-Path 'X:\Program Files\WindowsPowerShell\Modules\OSD' $osdlatest.version | Join-Path -ChildPath '\Public\OSDCloud\'
+(Get-Content $fullpath\Start-OSDCloud.ps1).Replace("OSVersion = 'Windows 10'","OSVersion = 'Windows 11'") | Out-File $fullpath\Start-OSDCloud.ps1 -ErrorAction SilentlyContinue
+
+## Start OSDCloud with Params
+Start-OSDCloud -Screenshot -SkipAutopilot -SkipODT -ZTI -OSBuild 21H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -Restart
