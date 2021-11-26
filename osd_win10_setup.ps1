0b0d9e4f8a0697ac31d841e2cd2bac90bf3754d5
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2021-11-26 10:36:44 +0100

    Update osd_win10_setup.ps1

--- osd_win10_setup.ps1
+++ osd_win10_setup.ps1
@@ -1,7 +1,20 @@
+## Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') 
+{
+Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
+Set-DisRes 1600
+} 
+
 ## change the default "Start-OSDCloud" behaviour from Windows 11 to Windows 10
 $osdlatest = Get-InstalledModule -Name OSD
 $fullpath = Join-Path 'X:\Program Files\WindowsPowerShell\Modules\OSD' $osdlatest.version | Join-Path -ChildPath '\Public\OSDCloud\'
 (Get-Content $fullpath\Start-OSDCloud.ps1).Replace("OSVersion = 'Windows 11'","OSVersion = 'Windows 10'") | Out-File $fullpath\Start-OSDCloud.ps1 -ErrorAction SilentlyContinue
 
 ## Start OSDCloud with Params
-Start-OSDCloud -Screenshot -SkipAutopilot -SkipODT -ZTI -OSBuild 21H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -Restart
+Write-Host -ForegroundColor Green "Start OSDCloud ZTI (Win10 Professional, Version 21H2, German)"
+Start-OSDCloud -OSLanguage de-de -OSBuild 21H2 -OSEdition Pro -ZTI
+
+##Restart from WinPE
+Write-Host -ForegroundColor Cyan "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
+wpeutil reboot
