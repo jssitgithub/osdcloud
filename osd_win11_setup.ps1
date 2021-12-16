61cead60836cf170c4a486781545c75ed62b9def
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2021-12-16 10:01:45 +0100

    Update osd_win11_setup.ps1

--- osd_win11_setup.ps1
+++ osd_win11_setup.ps1
@@ -4,4 +4,4 @@ $fullpath = Join-Path 'X:\Program Files\WindowsPowerShell\Modules\OSD' $osdlates
 (Get-Content $fullpath\Start-OSDCloud.ps1).Replace("OSVersion = 'Windows 10'","OSVersion = 'Windows 11'") | Out-File $fullpath\Start-OSDCloud.ps1 -ErrorAction SilentlyContinue
 
 ## Start OSDCloud with Params
-Start-OSDCloud -Screenshot -SkipAutopilot -SkipODT -ZTI -OSBuild 21H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -Restart
+Start-OSDCloud -SkipAutopilot -SkipODT -ZTI -OSBuild 21H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -Restart
