2c3f58d850aed3a48c2b2ae2d15e9c316e5e4c88
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2021-12-16 10:00:31 +0100

    Update osd_win10_setup.ps1

--- osd_win10_setup.ps1
+++ osd_win10_setup.ps1
@@ -11,8 +11,8 @@ $fullpath = Join-Path 'X:\Program Files\WindowsPowerShell\Modules\OSD' $osdlates
 (Get-Content $fullpath\Start-OSDCloud.ps1).Replace("OSVersion = 'Windows 11'","OSVersion = 'Windows 10'") | Out-File $fullpath\Start-OSDCloud.ps1 -ErrorAction SilentlyContinue
 
 ## Start OSDCloud with Params
-Write-Host -ForegroundColor Green "Start OSDCloud ZTI (Win10 Professional, Version 21H2, German)"
-Start-OSDCloud -OSLanguage de-de -OSBuild 21H2 -OSEdition Pro -ZTI
+Write-Host -ForegroundColor Green "Start OSDCloud ZTI (Win10 Professional, Version 21H2, English)"
+Start-OSDCloud -OSLanguage en-us -OSBuild 21H2 -OSEdition Pro -ZTI
 
 ##Restart from WinPE
 Write-Host -ForegroundColor Cyan "Restarting in 10 seconds!"
