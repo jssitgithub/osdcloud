9f5e9c3e5309ffa125aa882e87dcbffe3d0c8c26
jssitgithub <92300907+jssitgithub@users.noreply.github.com>
2024-04-10 11:08:37 +0200

    Update osd_win11_setup.ps1

--- osd_win11_setup.ps1
+++ osd_win11_setup.ps1
@@ -1,2 +1,2 @@
 ## Start OSDCloud with Params
-Start-OSDCloud -SkipAutopilot -SkipODT -ZTI -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail -Restart
+Start-OSDCloud -SkipAutopilot -SkipODT -ZTI -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -Restart
